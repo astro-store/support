3cfe8fd0e4ba518a348f77d15195500f6cc3b6ab
Arthur Minasyan <ArtMin96@users.noreply.github.com>
2023-02-09 18:16:22 +0400

    Commit

--- CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# Changelog
-
-All notable changes to `support` will be documented in this file.
--- tests/Pest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use AstroStore\Support\Tests\TestCase;
-
-uses(TestCase::class)->in(__DIR__);
